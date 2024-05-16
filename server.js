e4024ea28da13cc0a6f4e2fe0d091d06bd066769
Aiko <134149662+zosanmama@users.noreply.github.com>
2024-05-16 15:54:21 +0900

    20240516

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
